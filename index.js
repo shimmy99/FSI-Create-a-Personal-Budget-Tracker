503431ba6602547a1c4956702b44c0c68d9ba4b9
Drew Shimkowski <Drew>
2022-08-04 19:25:04 -0700

    quick udpate

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+    console.log(answer)
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
 // Your Code Here
